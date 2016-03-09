ab0ee57c035871db6197d8bc78d2debd006af566
Michael L Perry <michael@qedcode.com>
2016-03-09 06:58:00 -0600

    Added new main form.

--- BindingWithoutForms/BindingWithoutForms/BindingWithoutForms.Droid/MainActivity.cs
+++ BindingWithoutForms/BindingWithoutForms/BindingWithoutForms.Droid/MainActivity.cs
@@ -6,28 +6,24 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Assisticant.Binding;
 
-namespace BindingWithoutForms.Droid
+namespace Lister
 {
-	[Activity (Label = "BindingWithoutForms.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "Lister", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+		}
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
 		}
 	}
 }
--- BindingWithoutForms/BindingWithoutForms/BindingWithoutForms.Droid/Resources/Resource.designer.cs
+++ BindingWithoutForms/BindingWithoutForms/BindingWithoutForms.Droid/Resources/Resource.designer.cs
@@ -62,8 +62,14 @@ namespace BindingWithoutForms.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int buttonAdd = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int editName = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int listPeople = 2131034113;
 			
 			static Id()
 			{
