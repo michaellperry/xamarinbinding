89107a196446b6c12c4bb55689bf8113ed432cff
Michael L Perry <michael@qedcode.com>
2016-03-09 06:50:35 -0600

    Initial Projects.

---
+++ BindingWithoutForms/BindingWithoutForms/BindingWithoutForms.Droid/ViewModels/PersonViewModel.cs
@@ -0,0 +1,50 @@
+using BindingWithoutForms.Models;
+using System;
+
+namespace BindingWithoutForms.Droid.ViewModels
+{
+    public class PersonViewModel
+	{
+		private readonly Person _person;
+
+		public PersonViewModel(Person person)
+		{
+			_person = person;
+		}
+
+		public Person Person
+		{
+			get { return _person; }
+		}
+
+		public string Name
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(_person.Name))
+					return "<no name>";
+				else
+					return _person.Name;
+			}
+		}
+
+		public override bool Equals(Object obj)
+		{
+			if (obj == this)
+				return true;
+
+			var that = obj as PersonViewModel;
+			if (that == null)
+				return false;
+
+			return this._person == that._person;
+		}
+
+		public override int GetHashCode()
+		{
+			return _person.GetHashCode();
+		}
+	}
+
+}
+
---
+++ BindingWithoutForms/BindingWithoutForms/BindingWithoutForms/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Assisticant.Fields;
+
+namespace BindingWithoutForms.Models
+{
+	public class Person
+	{
+		private Observable<string> _name = new Observable<string>();
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name.Value = value; }
+		}
+	}
+}
+
